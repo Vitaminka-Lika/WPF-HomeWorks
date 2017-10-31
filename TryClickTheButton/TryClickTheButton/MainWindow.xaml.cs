80855baf99a826f57fcd619342c5fbc0eb8b5b2d
Vitaminka-Lika <sarbassova_alika2001@mail.ru>
2017-10-31 23:21:02 +0600

    13/10/17
    Создайте приложение, состоящее из окна с одной кнопкой, за которой бы пользователь «гонялся» по всему окну с помощью мыши, но не мог щелкнуть ее.

---
+++ TryClickTheButton/TryClickTheButton/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TryClickTheButton
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int safeHeight = 100;
+        int safeWidth = 100;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RunningButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            runningButton.IsEnabled = false;
+            Random random = new Random();
+            int left = random.Next(0, safeHeight);
+            int top = random.Next(0, safeWidth);
+            Canvas.SetLeft(runningButton, left);
+            Canvas.SetTop(runningButton, top);
+        }
+
+        private void RunningButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            runningButton.Content = "Click Me!!!";
+            runningButton.IsEnabled = true;
+        }
+
+        private void RunningButton_Click(object sender, RoutedEventArgs e)
+        {
+            runningButton.Content = "HOOOOOW???";
+        }
+    }
+}
