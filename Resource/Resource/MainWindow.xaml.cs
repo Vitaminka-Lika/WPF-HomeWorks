ed2d4137b181f8a3c462e2acd6531825b0920447
Vitaminka-Lika <sarbassova_alika2001@mail.ru>
2017-11-01 04:38:13 +0600

    30/10/17
    Создайте настраиваемый элемент управления, который будет воспроизводить изображение и автоматически извлекать из метаданных его имя, выводимое под изображением.

---
+++ Resource/Resource/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Resource
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
+            openDialog.ShowDialog();
+            mainImage.Source = new BitmapImage(new Uri(openDialog.FileName));
+            string name = mainImage.Source.ToString();
+            char symbol = '/';
+            int index = (name.LastIndexOf(symbol));
+            index++;
+            resourceTextBox.Text = name.Remove(0,index);
+        }
+    }
+}
